2641d703d7c9e04822659d8fb29df68203f29da8
Mehedi <mehedi.interface@gmail.com>
2026-02-26 22:59:56 +0600

    26-9 Looping objects: for...in, for...of

---
+++ loop.js
@@ -0,0 +1,18 @@
+const numbers = [1, 2, 3, 4, 5]; 
+
+for (const number of numbers) {
+    console.log(number); 
+}
+
+const employee = {
+    name: 'John Doe', 
+    1: 'desk one', 
+    'home-address': '123 Main St', 
+    salary: 50000, 
+    position: 'software engineer'
+}; 
+
+for (const key in employee) {
+    const value = employee[key]
+    console.log(key, value)
+}
